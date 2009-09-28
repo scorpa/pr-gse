2b630254f08394bba75e8ee13a7ce43a76ef3c98
rudolf.radlbauer <rudolf.radlbauer@666a50c4-ab49-11dd-a714-1752b6e4c161>
2009-09-28 11:51:01 +0000

    Tictactoe (intermediate state)

---
+++ beispiele/tictactoe/TictactoeException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tictactoe;
+
+/**
+ *
+ * @author Rudi
+ */
+public class TictactoeException extends Exception
+{
+
+    public TictactoeException(String message)
+    {
+        super(message);
+    }
+
+}
