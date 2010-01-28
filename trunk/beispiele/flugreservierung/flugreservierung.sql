bcd0964e7d157590a3805e7e98441585d3bb584e
rudolf.radlbauer <rudolf.radlbauer@666a50c4-ab49-11dd-a714-1752b6e4c161>
2010-01-28 11:28:05 +0000

    Flugreservierung

--- trunk/beispiele/flugreservierung/flugreservierung.sql
+++ trunk/beispiele/flugreservierung/flugreservierung.sql
@@ -1,18 +1,18 @@
 
-CREATE TABLE IF NOT EXISTS `fluege` (
-  `flugnummer` int(11) NOT NULL auto_increment,
-  `von` varchar(128) NOT NULL,
-  `nach` varchar(128) NOT NULL,
-  `start` datetime NOT NULL,
-  `ankunft` datetime NOT NULL,
-  `sitzplaetze` int(11) NOT NULL,
-  PRIMARY KEY  (`flugnummer`)
+CREATE TABLE IF NOT EXISTS fluege (
+  flugnummer int(11) NOT NULL auto_increment,
+  von varchar(128) NOT NULL,
+  nach varchar(128) NOT NULL,
+  abflug datetime NOT NULL,
+  ankunft datetime NOT NULL,
+  sitzplaetze int(11) NOT NULL,
+  PRIMARY KEY  (flugnummer)
 )
 
 
-CREATE TABLE IF NOT EXISTS `reservierungen` (
-  `reservierungsnummer` int(11) NOT NULL auto_increment,
-  `flugnummer` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  PRIMARY KEY  (`reservierungsnummer`)
+CREATE TABLE IF NOT EXISTS reservierungen (
+  reservierungsnummer int(11) NOT NULL auto_increment,
+  flugnummer int(11) NOT NULL,
+  passagier_name varchar(128) NOT NULL,
+  PRIMARY KEY  (reservierungsnummer)
 )
