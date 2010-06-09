8036083c746110e1cbde0d53bb5817e247f54cc5
rudolf.radlbauer <rudolf.radlbauer@666a50c4-ab49-11dd-a714-1752b6e4c161>
2010-06-09 08:18:32 +0000

    quiz

---
+++ uebungen/musterloesungen/src/quiz/Frage.java
@@ -0,0 +1,51 @@
+package quiz;
+
+/**
+ *
+ * @author Rudolf Radlbauer
+ */
+public class Frage
+{
+    private int id;
+    private String frage;
+    private String[] antworten = new String[4];
+    private int richtig;
+
+    public Frage(int id)
+    {
+        this.id = id;
+    }
+
+    public void setAntwort(int index, String antwort)
+    {
+        antworten[index] = antwort;
+    }
+
+    public String getAntwort(int index)
+    {
+        return antworten[index];
+    }
+
+    public String getFrage()
+    {
+        return frage;
+    }
+
+    public void setFrage(String frage)
+    {
+        this.frage = frage;
+    }
+
+    public int getRichtig()
+    {
+        return richtig;
+    }
+
+    public void setRichtig(int richtig)
+    {
+        this.richtig = richtig;
+    }
+
+
+
+}
