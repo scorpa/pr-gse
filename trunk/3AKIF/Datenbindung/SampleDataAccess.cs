13bde1c6d75802a55bbac371e642456f773555b2
rudolf.radlbauer <rudolf.radlbauer@666a50c4-ab49-11dd-a714-1752b6e4c161>
2012-10-16 12:40:01 +0000

    Datenbindung Beispiel

---
+++ trunk/3AKIF/Datenbindung/SampleDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqInAction.LinqBooks.Common;
+
+namespace DataSampleBinding
+{
+    public class SampleDataAccess
+    {
+        public static Publisher[] DPublishers
+        {
+            get { return SampleData.Publishers; }
+        }
+
+        public static Author[] DAuthors
+        {
+            get { return SampleData.Authors; }
+        }
+
+        public static Book[] DBooks
+        {
+            get { return SampleData.Books; }
+        }
+
+        public static Subject[] DSubjects
+        {
+            get { return SampleData.Subjects; }
+        }
+
+        public static Review[] DReviews
+        {
+            get { return SampleData.Reviews; }
+        }
+
+        public static User[] DUsers
+        {
+            get { return SampleData.Users; }
+        }
+    }
+
+}
